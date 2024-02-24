59c4b3a8ad75a1cbaaaebefc2dd45d97366b27a1
Morgan <msswanso@calpoly.edu>
2024-02-24 14:35:05 -0800

    working

--- software/consumer/kafka_gps_consumer.py
+++ software/consumer/kafka_gps_consumer.py
@@ -5,6 +5,9 @@ import sys
 import re
 import struct
 import argparse
+import os
+import json
+import datetime
 
 import avro.schema
 import avro.io
@@ -17,15 +20,17 @@ topic = 'remote'
 if __name__ == "__main__":
 
     # avro schema path
-    schema_path = '../schema/gps.avsc'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    schema_path = f"{str(dir_path)}/../schema/gps.avsc"
 
     # load avro schema
     schema = avro.schema.parse(open(schema_path).read())
 
-    consumer = KafkaConsumer(topic, group_id=None)
+    consumer = KafkaConsumer(topic, bootstrap_servers=["localhost:9092"], group_id=None, \
+                             auto_offset_reset="earliest")
 
     for message in consumer: 
-        key_splited = message.key.split(':')
+        key_splited = message.key.decode("utf-8").split(':')
         if key_splited[0] != 'gps':
             continue
 
@@ -36,6 +41,18 @@ if __name__ == "__main__":
         decoder = avro.io.BinaryDecoder(bytes_reader)
         reader = avro.io.DatumReader(schema)
         gps_datum = reader.read(decoder)
-
-        print gps_datum
-        print ''
+        time = gps_datum['object'].get('time')
+        lat = gps_datum['object'].get('lat')
+        lon = gps_datum['object'].get('lon')
+        alt = gps_datum['object'].get('alt')
+        if time and lat and lon and alt:
+            dt = datetime.datetime.fromtimestamp(time)
+            formatted_time = dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+            payload = { "timestamp": formatted_time,
+                        "gps": {"altitude": alt,
+                                "latitude": lat,
+                                "longitude": lon,
+                                "nmea": [],
+                                "utc": "" }
+                       }
+            print(json.dumps(payload))
